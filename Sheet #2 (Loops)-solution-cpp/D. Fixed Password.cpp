77dbcb3798f91ef85db1b6c3cffe3d3d5cbf06d1
Md. Abdul Wahab <abdulwahab22400@gmail.com>
2024-06-27 10:20:23 +0600

    27/06/2024

---
+++ Sheet #2 (Loops)-solution-cpp/D. Fixed Password.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int X;
+    while(cin>>X){
+        if(X==1999){
+            cout<<"Correct"<<"\n";
+            break;
+        }
+        else{
+            cout<<"Wrong"<<"\n";
+        }
+    }
+}
+
+
+
