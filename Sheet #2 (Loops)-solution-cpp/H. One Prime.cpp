7457f0d2feedb5bcd1423dab656052c26759c937
Md. Abdul Wahab <abdulwahab22400@gmail.com>
2024-06-28 12:50:46 +0600

    28/06/2024

---
+++ Sheet #2 (Loops)-solution-cpp/H. One Prime.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+bool checkPrime(int n)
+{
+    bool isPrime=true;
+    if(n==1 || n==0)
+    {
+        return isPrime=false;
+    }
+    for(int i=2; i<=n/2; ++i)
+    {
+        if(n%i==0)
+        {
+            return isPrime=false;
+            break;
+
+        }
+    }
+    return isPrime;
+}
+
+int main()
+{
+    int n,temp;
+    cin>>n;
+    if(checkPrime(n))
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+}
+
