79db0a2020cb579b9e19f0b5eaa2e9bb366eae69
MRiDuL-ICE <abdulwahab22400@gmail.com>
2024-07-03 16:57:03 +0600

    03/07/2024

---
+++ Sheet #2 (Loops)-solution-cpp/N. Numbers Histogram.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin>>S;
+    int N,X;
+    cin>>N;
+    for(int i=1; i<=N; i++)
+    {
+        cin>>X;
+        for(int j=1; j<=X; j++)
+        {
+            cout<<S;
+        }
+        cout<<"\n";
+    }
+}
